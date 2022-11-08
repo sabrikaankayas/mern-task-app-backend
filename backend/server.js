4262de6472113751e4bb7a09ccb9d0e1a6c94381
sabrikaankayas <sabrikaankayas@gmail.com>
2022-11-08 06:20:13 +0300

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const taskRoutes = require("./routes/taskRoute")
+
+
+const app = express()
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors(
+    {origin: ["http://localhost:3000/", "https://task-manager-mern-app.onrender.com"]
+}))
+app.use("/api/tasks", taskRoutes)
+
+
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+    })
+    .catch((err) => console.log(err))
+
+
+
+// startServer()
